414bbb592f015b9a3c5248719c483f0e585c5a03
AlexanderBlackman <51733876+AlexanderBlackman@users.noreply.github.com>
2022-06-23 13:44:16 +1000

    Add project files.

---
+++ PeriodicQuestions/ActiveWindow/PeriodicWindowWatcher.cs
@@ -0,0 +1,41 @@
+﻿using PeriodicQuestions.Helpers;
+using System;
+using System.Threading;
+
+namespace PeriodicQuestions.ActiveWindow
+{
+    public class PeriodicWindowWatcher
+    {
+        private PeriodicTimer stateTimer;
+
+
+
+
+        private ActiveWindowModel currentActiveWindow = ActiveWindowModel.CreateEmpty();
+        public event EventHandler<ActiveWindowChangedEventArgs> ActiveWindowChanged;
+
+        public PeriodicWindowWatcher(TimeSpan interval)
+        {
+            InitialiseTimer(interval);
+        }
+
+        public async void InitialiseTimer(TimeSpan interval)
+        {
+            stateTimer = new PeriodicTimer(interval);
+            while (await stateTimer.WaitForNextTickAsync())
+            {
+                GetActiveWindow();
+            }
+        }
+
+        private void GetActiveWindow() =>
+            WindowAPI
+                .GetActiveWindowTitle()
+                .Do(activeWindow =>
+                    activeWindow.IsDifferentThan(currentActiveWindow, () =>
+                    {
+                        currentActiveWindow = ActiveWindowModel.CreateFrom(activeWindow);
+                        ActiveWindowChanged?.Invoke(this, ActiveWindowChangedEventArgs.Create(activeWindow.WindowTitle));
+                    }));
+    }
+}
---
+++ PeriodicQuestions/QuestionWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using WinUIEx;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace PeriodicQuestions
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class QuestionWindow : WindowEx
+    {
+        public QuestionWindow()
+        {
+            this.InitializeComponent();
+            IsTitleBarVisible = false;
+            IsMinimizable = false;
+            IsMaximizable = false;
+            this.CenterOnScreen();
+            IsAlwaysOnTop = true;
+        }
+    }
+}
